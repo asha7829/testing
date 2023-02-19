528a943961d098b061fd1ff229f4505b8c4dec19
Asha <Asha@DESKTOP-KOMBKNR>
2023-02-19 17:00:10 +0530

    samplevivek

---
+++ selenium/src/hello/sample.py
@@ -0,0 +1,19 @@
+'''
+Created on 19-Feb-2023
+
+@author: Asha
+'''
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#import time
+#from selenium.webdriver import ActionChains
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+#options.add_argument("start-maximized")
+#options.add_argument("incognito")
+driver = webdriver.Chrome(options=options)
+#driver.get("http://www.google.com")
+driver.get("https://testautomationpractice.blogspot.com/")
+driver.maximize_window()
+driver.find_element(By.XPATH,".//span[@class='ui-slider-handle ui-corner-all ui-state-default']")
+driver.execute_script("window.scrollBy(0,900)","")
